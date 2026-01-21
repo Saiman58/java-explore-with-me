6b69d24ed4d9ad86189bde288812db2c4c2db0b3
Alex <radist.al@yandex.ru>
2026-01-22 01:14:55 +0300

    Diploma project (module 2) Development of the main service

--- ewm-main/server/src/main/java/ru/practicum/explorewithme/server/service/CategoryServiceImpl.java
+++ ewm-main/server/src/main/java/ru/practicum/explorewithme/server/service/CategoryServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.practicum.explorewithme.server.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
