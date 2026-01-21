eeeaec2054b06408f1f78ff94cf5eebe58532b82
Alex <radist.al@yandex.ru>
2026-01-22 01:09:36 +0300

    Diploma project (module 2) Development of the main service

--- ewm-main/server/src/main/java/ru/practicum/explorewithme/server/exception/MissingServletRequestParameterException.java
+++ ewm-main/server/src/main/java/ru/practicum/explorewithme/server/exception/MissingServletRequestParameterException.java
@@ -1,7 +1,7 @@
 package ru.practicum.explorewithme.server.exception;
 
 public class MissingServletRequestParameterException extends RuntimeException {
-    public MissingServletRequestParameterException(String message){
+    public MissingServletRequestParameterException(String message) {
         super(message);
     }
 }
