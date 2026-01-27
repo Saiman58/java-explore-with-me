01e9bc1047136e2500337af9b551b9103c949c7f
Alex <radist.al@yandex.ru>
2026-01-27 22:57:55 +0300

    Diploma project (module 3)
    Feature comments

--- ewm-main/dto/src/main/java/ru/practicum/explorewithme/comment/dto/CommentDto.java
+++ ewm-main/dto/src/main/java/ru/practicum/explorewithme/comment/dto/CommentDto.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.explorewithme.event.dto.EventShortDto;
-import ru.practicum.explorewithme.user.dto.UserShortDto;
 
 import java.time.LocalDateTime;
 
